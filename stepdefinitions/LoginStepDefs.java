7737b30eedbfaa1c086869af76f3ec50a90e9f95
Baris Can Ateş <103248330+Bariscanates404@users.noreply.github.com>
2022-06-13 00:29:03 +0300

    LoginStepDefs.java
    LoginStepDefs.java

--- stepdefinitions/LoginStepDefs.java
+++ stepdefinitions/LoginStepDefs.java
@@ -26,17 +26,8 @@ public class LoginStepDefs {
 
     @Then("kullanici dogru email ve sifreyi girer")
     public void kullanici_dogru_email_ve_sifreyi_girer(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-      List<String> loginCredentials = dataTable.row(1); // datatable row lari 1 den basalar
-        // datatable in credential lari  bir nevi  sifirinci satirdadir diyebiliriz..
-        // data table daki 1. satir daki ilk eleman 0. index gibi alinir.. yanindaki de 1. idexdir..
-        // feature dosyasinda hangi methoda aitse data table daki datalari cagirmak icin bu sekilde kullanim mevcuttur
+        
+      List<String> loginCredentials = dataTable.row(1); 
       loginPage.loginemailElement.sendKeys(loginCredentials.get(0)); //practicebatch44@gmail.com
       loginPage.loginPasswordBox.sendKeys(loginCredentials.get(1)); //123456
 
@@ -65,11 +56,7 @@ public class LoginStepDefs {
 
     @Then("kullanici dogru email ve sifreyi excel ile girer")
     public void kullaniciDogruEmailVeSifreyiExcelIleGirer() {
-    //   String path = "src/test/resources/testData/testData.xlsx";
-    //   String sheetName = "Sayfa1";
-    //   ExcelUtil excelUtil =new ExcelUtil(path,sheetName);
-
-    //   List<Map<String, String>> loginData = excelUtil.getDataList();
+    
 
         loginPage.loginemailElement.sendKeys(loginData.get(0).get("Email"));
         loginPage.loginPasswordBox.sendKeys(loginData.get(0).get("Password"));
